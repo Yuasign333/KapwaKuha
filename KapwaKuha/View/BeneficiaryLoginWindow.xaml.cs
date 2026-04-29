2b14db280fda2461fcd16e994fadf6545eb8aa31
yuanm <yuanm@DESKTOP-8P1VJSE>
2026-04-29 11:30:43 +0800

    more box refinement ui

--- KapwaKuha/View/BeneficiaryLoginWindow.xaml.cs
+++ KapwaKuha/View/BeneficiaryLoginWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using KapwaKuha.ViewModels;
 using KapwaKuha.Services;
 using System.Windows;
+using System.Windows.Controls; 
 
 namespace KapwaKuha.View
 {
@@ -15,13 +16,18 @@ namespace KapwaKuha.View
         private void PwBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
             // If the password box has text, hide the placeholder. Otherwise, show it.
-            if (string.IsNullOrEmpty(PwBox.Password))
+            var placeholder = this.FindName("PlaceholderText") as UIElement;
+            var pwBox = sender as PasswordBox;
+            if (placeholder != null && pwBox != null)
             {
-                PlaceholderText.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                PlaceholderText.Visibility = Visibility.Collapsed;
+                if (string.IsNullOrEmpty(pwBox.Password))
+                {
+                    placeholder.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    placeholder.Visibility = Visibility.Collapsed;
+                }
             }
         }
     }
